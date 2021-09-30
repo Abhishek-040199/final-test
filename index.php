1512a8a1b8e90123db36968cab844fa2fb177767
Abhishek-040199 <90102336+Abhishek-040199@users.noreply.github.com>
2021-10-01 02:30:42 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-
-include_once("home (1).html");
-
-?>
+         /* Defining a PHP Function */
+         function writeMessage() {
+            echo "You are really a nice person, Have a nice time!";
+            $username=username;
+            $password=password;
+            $yourl="http://52.66.204.194:8000/$password/$username"
+            $html = file_get_html($yourl);     
+         }    
+         echo $html -> plaintext;
+         /* Calling a PHP Function */
+         writeMessage();
+      ?>
